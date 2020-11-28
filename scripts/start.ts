4b71ae2d5340f21d0557a3a266925aec373f963f
Phil Hill <phil@technobuddha.com>
2020-11-27 23:57:57 -0500

    Grab stuff from create-react-app

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,226 +1,222 @@
-#!/usr/bin/env ts-node
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV  = 'development';
 
-import process                      from 'process';
-import webpack                      from 'webpack';
-import commandLineArgs              from 'command-line-args';
-import commandLineUsage             from 'command-line-usage';
-import chalk                        from 'chalk';
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+// import getClientEnvironment from '../config/env';
+
+// import fs from 'fs';
+import webpack from 'webpack';
+// import WebpackDevServer from 'webpack-dev-server';
+//import clearConsole from 'react-dev-utils/clearConsole';
+// import checkRequiredFiles from 'react-dev-utils/checkRequiredFiles';
+// import {
+//   choosePort,
+//   createCompiler,
+//   prepareProxy,
+//   prepareUrls,
+//} from 'react-dev-utils/WebpackDevServerUtils';
+//import openBrowser from 'react-dev-utils/openBrowser';
+//import semver from 'semver';
+//import paths from '../config/paths';
+//import configFactory from '../config/webpack.config';
+//import createDevServerConfig from '../config/webpackDevServer.config';
+//import react from 'react';//require.resolve('react', { paths: [paths.appPath] });
+
+//import { checkBrowsers } from 'react-dev-utils/browsersHelper';
+
+///
 import repeat                       from 'lodash/repeat';
-import { spawn, ChildProcess }      from 'child_process';
-import { genServerWebpackConfig }   from '../src/server/webpack.config';
-import { genClientWebpackConfig }   from '../src/client/webpack.config';
+import chalk from 'chalk'; chalk.level = 3;    // Tell chalk that we support full RGB colors
+import { out, clearScreen, header, screenSize } from '@technobuddha/vt100';
+import { spawn, ChildProcess }                  from 'child_process';
+import { genServerWebpackConfig }               from '../src/server/webpack.config';
 
-chalk.level = 3;    // Tell chalk that we support full RGB colors
+let serverProcess: ChildProcess | null = null;
 
-//#region Terminal functions
-const esc   = '\u001b';
-
-function out(text: string) {
-    process.stdout.write(text);
-}
-
-function clearScreen() {
-    out(`${esc}[2J`);
-}
-
-function setScrollRegion(begin?: number, end?: number) {
-    if(begin && end) 
-        out(`${esc}[${begin};${end}r`);
-    else
-        out(`${esc}[r`);
-}
-
-function moveCursorTo(x: number, y: number) {
-    out(`${esc}[${y};${x}H`);
-}
-//#endregion
-//#region Logo
-const HEADER_HEIGHT = 9;
-function header() {
-    const logo = [
-        chalk.hex('#d0f2fa')('▄██▄'),
-        chalk.hex('#a3e1f6')('▄██████▄'),
-        chalk.hex('#70c0e4')('▄██████████▄'),
-        chalk.hex('#4b98ca')('▄██████████████▄'),
-        chalk.hex('#1b5ca8')('▄██████████████████▄'),
-        chalk.hex('#135490')('▄██████████████████████▄'),
-        chalk.hex('#0d3f78')('▄██████████████████████████▄'),
-        chalk.hex('#082c61')('▄██████████████████████████████▄'),
-        chalk.hex('#051f50')('▄██████████████████████████████████▄'),
-    ]
-
-    const name = [
-        '',
-        '',
-        'H     H  IIIIIII   L         L',
-        'H     H     I      L         L',
-        'H     H     I      L         L',
-        'HHHHHHH     I      L         L',
-        'H     H     I      L         L',
-        'H     H     I      L         L',
-        'H     H  IIIIIII   LLLLLLL   LLLLLLL',
-    ]
-
-    let output = "";
-    for(let i = 0; i < logo.length; ++i) {
-        for(let j = 8; j > i; --j) {
-            output += '  '
-        }
-        output += logo[i];
-        for(let j = 8; j > i; --j) {
-            output += '  '
-        }
-        output += '   ' + name[i] + '\n';
+const startServer   = () => {
+    if(serverProcess) {
+        out(chalk.red(`\nServer changed, restarting...\n\n`));
+        stopServer();
     }
-    return output;
-}
-//#endregion
-//#region Command Line Parsing
-const options: commandLineArgs.CommandLineOptions = commandLineArgs(
-    [{
-        name:           'target',
-        alias:          't',
-        defaultOption:  true,
-        defaultValue:   'development',
-        type:           (target: string) => {
-            if(target === 'development' || target === 'production')
-                return target;
-            else
-                throw Error('--target must be one of "development" or "production"');
-        },
-    },
-    {
-        name:           'help',
-        alias:          'h',
-        type:           Boolean,
-    }],
-    { stopAtFirstUnknown: true }
-);
 
-if(options.help) {
-    out(
-        commandLineUsage([
-            {
-                content: header(),
-                raw: true,
-            },
-            {
-                header: 'Technobuddha development tool',
-                content: 'A tool for debugging and deploying Technobuddha\'s web application',
-            },
-            {
-                header: 'Options',
-                optionList: [
-                    {
-                        name: 'target, -t',
-                        typeLabel: '{bold development} | {bold production}',
-                        description: 'The build target'
-                    },
-                    {
-                        name: 'help, -h',
-                        typeLabel: ' ',
-                        description: 'Display this usage guide.'
-                    }
-                ]
-            }
-        ])
-    );
+    serverProcess = spawn('node', ['./bin/server.js'], { stdio: 'inherit'});
+    //serverProcess?.stdout?.on('data', out);
+    //serverProcess?.stderr?.on('data', out);
+}
+
+const stopServer    = () => {
+    if(serverProcess) {
+        out(chalk.red(`\nServer shutting down...\n\n`));
+        serverProcess.kill();
+        serverProcess = null;
+    }
+}
+
+const exit = () => {
+    stopServer();
+
+ //    setScrollRegion();
+ //    moveCursorTo(1, height);
     process.exit(0);
 }
-//#endregion
 
-if(options.target === 'development') {
-    let   height                                      = process.stdout.rows;
-    let   width                                       = process.stdout.columns;
-    let   serverProcess: ChildProcess | null          = null;
+['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGQUIT', 'exit', 'uncaughtexception'].forEach(
+    sig => process.on(sig, exit)
+);
 
-    if(height && width) {
-        clearScreen();
-        setScrollRegion(HEADER_HEIGHT + 2, height);   // +1 for the row of ===, +1 to begin after the logo
-        out(header());
-        out(`${repeat('.', width)}\n`);
-        moveCursorTo(1, height);
-
-        process.stdout.on(
-            'resize',
-            () => {
-                height = process.stdout.rows;
-                width  = process.stdout.columns;
-
-                clearScreen();
-                setScrollRegion();
-                moveCursorTo(1, 1);
-                out(header());
-                out(`${repeat('=', width)}\n`);
-                setScrollRegion(HEADER_HEIGHT + 2, height);
-                moveCursorTo(1, height);
-            }
-        );
+webpack(genServerWebpackConfig(true)).watch(
+    {},
+    (error: Error, stats: webpack.Stats) => {
+       stopServer();
+       if(error ?? stats.hasErrors())
+           process.stdout.write(`${stats.toString('errors-only')}\n`);
+       else
+           startServer();
     }
+);
 
-    const startServer   = () => {
-        if(serverProcess) {
-            out(chalk.red(`Server changed, restarting...\n`));
-            stopServer();
-        }
+const {width} = screenSize();
 
-        serverProcess = spawn('node', ['./bin/server.js']);
-        serverProcess?.stdout?.on('data', out);
-        serverProcess?.stderr?.on('data', out);
-    }
-    
-    const stopServer    = () => {
-        if(serverProcess) {
-            serverProcess.kill();
-            serverProcess = null;
-        }
-    }
-    
-    const exit = () => {
-        stopServer();
+clearScreen();
+// setScrollRegion(HEADER_HEIGHT+2, height);
+out(header());
+out(`${repeat('=', width)}\n`)
 
-        setScrollRegion();
-        moveCursorTo(1, height);
-        process.exit(0);
-    }
+/*
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+const useYarn = fs.existsSync(paths.yarnLockFile);
+const isInteractive = process.stdout.isTTY;
 
 
-    process.on('SIGINT', exit);
-    process.on('SIGTERM', exit);
-    process.on('SIGHUP', exit);
-    process.on('SIGQUIT', exit);
-    process.on('exit', exit);
-    process.on('uncaughtException', exit);
-
-    clearScreen();
-    setScrollRegion(HEADER_HEIGHT+2, height);
-    out(header());
-    out(`${repeat('=', width)}\n`)
-
-    webpack(genServerWebpackConfig(true)).watch(
-        {},
-        (error: Error, stats: webpack.Stats) => {
-           stopServer();
-           if(error ?? stats.hasErrors())
-               process.stdout.write(`${stats.toString('errors-only')}\n`);
-           else
-               startServer();
-        }
-    );
-
-} else {
-    webpack(
-        genServerWebpackConfig(false),
-        (_error: Error, _stats: webpack.Stats) => {
-            out('--client');
-            webpack(
-                genClientWebpackConfig(false),
-                (_error: Error, _stats: webpack.Stats) => {
-                    out('--done');
-                }
-            )
-        }
-    );
+// Warn and crash if required files are missing
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+  process.exit(1);
 }
 
+// Tools like Cloud9 rely on this.
+const DEFAULT_PORT = parseInt(process.env.PORT ?? '3000', 10);
+const HOST = process.env.HOST || '0.0.0.0';
 
+if (process.env.HOST) {
+    console.log(
+        chalk.cyan(
+            `Attempting to bind to HOST environment variable: ${chalk.yellow(chalk.bold(process.env.HOST))}`
+        )
+    );
+    console.log(
+        `If this was unintentional, check that you haven't mistakenly set it in your shell.`
+    );
+    console.log(
+        `Learn more here: ${chalk.yellow('https://cra.link/advanced-config')}`
+    );
+    console.log();
+}
+
+// We require that you explicitly set browsers and do not fall back to
+// browserslist defaults.
+
+checkBrowsers(paths.appPath, isInteractive)
+  .then(() => {
+    // We attempt to use the default port but if it is busy, we offer the user to
+    // run on a different port. `choosePort()` Promise resolves to the next free port.
+    return choosePort(HOST, DEFAULT_PORT);
+  })
+  .then((port: number | null) => {
+    if (port == null) {
+      // We have not found a port.
+      return;
+    }
+
+    const config = configFactory('development');
+    const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+    const appName = require(paths.appPackageJson).name;
+
+    const useTypeScript = fs.existsSync(paths.appTsConfig);
+    const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
+    const urls = prepareUrls(
+      protocol,
+      HOST,
+      port,
+      paths.publicUrlOrPath.slice(0, -1)
+    );
+    const devSocket = {
+      warnings: (warnings: string) =>
+        devServer.sockWrite(devServer.sockets, 'warnings', warnings),
+      errors: (errors: string) =>
+        devServer.sockWrite(devServer.sockets, 'errors', errors),
+    };
+    // Create a webpack compiler that is configured with custom messages.
+    const compiler = createCompiler({
+      appName,
+      config,
+      devSocket,
+      urls,
+      useYarn,
+      useTypeScript,
+      tscCompileOnError,
+      webpack,
+    });
+    // Load proxy config
+    const proxySetting = require(paths.appPackageJson).proxy;
+    const proxyConfig = prepareProxy(
+      proxySetting,
+      paths.appPublic,
+      paths.publicUrlOrPath
+    );
+    // Serve webpack assets generated by the compiler over a web server.
+    const serverConfig = createDevServerConfig(
+      proxyConfig,
+      urls.lanUrlForConfig
+    );
+    const devServer = new WebpackDevServer(compiler, serverConfig);
+    // Launch WebpackDevServer.
+    devServer.listen(port, HOST, err => {
+      if (err) {
+        return console.log(err);
+      }
+      if (isInteractive) {
+        clearConsole();
+      }
+
+      if (env.raw.FAST_REFRESH && semver.lt(react.version, '16.10.0')) {
+        console.log(
+          chalk.yellow(
+            `Fast Refresh requires React 16.10 or higher. You are using React ${react.version}.`
+          )
+        );
+      }
+
+      console.log(chalk.cyan('Starting the development server...\n'));
+      openBrowser(urls.localUrlForBrowser);
+    });
+
+    ['SIGINT', 'SIGTERM'].forEach(function (sig) {
+      process.on(sig, function () {
+        devServer.close();
+        process.exit();
+      });
+    });
+
+    if (process.env.CI !== 'true') {
+      // Gracefully exit when stdin ends
+      process.stdin.on('end', function () {
+        devServer.close();
+        process.exit();
+      });
+    }
+  })
+  .catch(err => {
+    if (err && err.message) {
+      console.log(err.message);
+    }
+    process.exit(1);
+  });
+*/

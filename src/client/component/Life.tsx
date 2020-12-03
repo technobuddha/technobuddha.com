3edaebfc6d8eeddc895929035d6feb08afb89712
Phil Hill <phil@technobuddha.com>
2020-12-02 22:14:40 -0500

    Improved performance life

--- src/client/component/Life.tsx
+++ src/client/component/Life.tsx
@@ -1,59 +1,37 @@
 import React                                from 'react';
-import clsx                                 from 'clsx';
 import { create2DArray }                    from '@technobuddha/library/create2DArray';
 import { useDerivedState, useDerivedValue } from '@technobuddha/react-hooks';
 import { Size }                             from 'mui-size';
-import { makeStyles }                       from '@material-ui/core/styles';
 
 const MOVES = [[1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1], [0,1]];
-const SIZE  = 4;
-
-const useLifeStyles = makeStyles(theme => ({
-    board: {
-        width: '100%',
-        height: '100%',
-        border: `1px solid ${theme.palette.primary.main}`,
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-    },
-    column: { 
-        display: 'flex',
-        flexDirection:'column',
-    },
-    square: {
-        width: SIZE,
-        height: SIZE,
-        minWidth: SIZE,
-        minHeight: SIZE,
-        //borderLeft: `1px solid ${theme.palette.primary.light}`,
-        //borderBottom: `1px solid ${theme.palette.primary.light}`,
-    },
-    life: {
-        background: theme.palette.primary.main,
-    },
-}));
+const SIZE  = 8;
 
 export const Life: React.FC = () => {
     return (
         <Size width="100%" height="100%">
-            {({width, height}) => <LifeBoard boxWidth={width} boxHeight={height} />}
+            {
+                ({width, height}) => 
+                    <LifeBoard boxWidth={width} boxHeight={height} />
+            }
         </Size>
     )
 }
 
-type LifeBoardProps = {boxWidth: number, boxHeight: number};
+type LifeBoardProps = {
+    boxWidth: number;
+    boxHeight: number;
+    children?: never;
+};
 
 const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardProps) => {
-    const css                   = useLifeStyles();
-    const div                   = React.useRef<HTMLDivElement>(null);
+    const canvas                = React.useRef<HTMLCanvasElement>(null);
     const width                 = useDerivedValue(() => Math.floor(boxWidth / SIZE),  [boxWidth]);
     const height                = useDerivedValue(() => Math.floor(boxHeight / SIZE), [boxHeight])
     const [move,  setMove]      = React.useState(0);
+
     const [board, setBoard]     = useDerivedState(
         () => {
             const b = create2DArray(width, height, false);
-            console.log(width, height, b);
             b[58][50] = true;
             b[59][50] = true;
             b[57][51] = true;
@@ -63,37 +41,13 @@ const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardPro
         },
         [width, height]
     );
-    const ui                    = useDerivedValue(
-        () => {
-            const b = create2DArray(height, width, () => {
-                const element = document.createElement('div');
-                return element;
-            });
-            return b;
-        },
-        [width, height]
-    );
-
-    React.useEffect(
-        () => {
-            ui.forEach(
-                row => {
-                    row.forEach(
-                        element => {
-                            div.current!.appendChild(element);
-                        }
-                    )
-                }
-            )
-        },
-        [false]
-    )
 
     React.useEffect(
         () => {
             const timer = setTimeout(
                 () => {
                     if(move < 1000) {
+                        const context   = canvas.current!.getContext('2d')!;
                         const nextBoard = create2DArray(width, height, false);
                         board.forEach(
                             (row, i) => {
@@ -110,13 +64,11 @@ const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardPro
                                         );
 
                                         switch(neighbors) {
-                                            case 0:
-                                            case 1:
-                                            case 4:
-                                            case 5:
-                                            case 6:
-                                            case 7:
-                                            case 8:
+                                            case 0: case 1: case 4: case 5: case 6: case 7: case 8:
+                                                if(cell) {
+                                                    context.fillStyle = 'white';
+                                                    context.fillRect(i * SIZE, j * SIZE, SIZE, SIZE);
+                                                }
                                                 nextBoard[i][j] = false;
                                                 break;
 
@@ -125,6 +77,10 @@ const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardPro
                                                 break;
 
                                             case 3:
+                                                if(!cell) {
+                                                    context.fillStyle = 'black';
+                                                    context.fillRect(i * SIZE, j * SIZE, SIZE, SIZE);    
+                                                }
                                                 nextBoard[i][j] = true;
                                                 break;
                                         }
@@ -132,16 +88,6 @@ const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardPro
                                 )
                             }
                         );
-
-                        nextBoard.forEach(
-                            (row, i) => {
-                                row.forEach(
-                                    (cell, j) => {
-                                        ui[j][i].className = clsx(css.square, cell && css.life)
-                                    }
-                                )
-                            }
-                        )
 
                         setBoard(nextBoard);
                         setMove(move => move + 1);
@@ -156,8 +102,8 @@ const LifeBoard: React.FC<LifeBoardProps> = ({boxWidth, boxHeight}: LifeBoardPro
     )
 
     return (
-        <div ref={div} className={css.board}>
-        </div>
+        <canvas ref={canvas} width={boxWidth} height={boxHeight}>
+        </canvas>
     );
 }
 

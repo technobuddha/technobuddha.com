45e0b1453eea40698eb72751c5eca24fccaf07fa
Phil Hill <phil@technobuddha.com>
2020-12-27 23:14:36 -0500

    Move page to config

---
+++ src/context/pages.tsx
@@ -0,0 +1,32 @@
+import React                        from 'react';
+import {useTranslation}             from '#context/i18n';
+import pages                        from '#settings/pages';
+import type { Page }                from '#settings/pages';
+
+export type { Page } from '#settings/pages';
+
+const PagesContext = React.createContext<Page[]>(null!);
+
+export function usePages() {
+   return React.useContext(PagesContext);
+}
+
+type PagesProviderProps = {
+    children?: React.ReactNode;
+}
+
+export const PagesProvider: React.FC = ({children}: PagesProviderProps = {}) => {
+    const { t, i18n } = useTranslation();
+    const control     = React.useMemo<Page[]>(() => 
+        pages.map(page => ({ ...page, primary: t(page.primary), secondary: page.secondary ? t(page.secondary) : undefined })),
+        [i18n.language]
+    );
+
+    return (
+        <PagesContext.Provider value={control}>
+            {children}
+        </PagesContext.Provider>
+    )
+}
+
+export default usePages;
--- src/context/pages/index.ts
+++
@@ -1,1 +0,0 @@
-export { default, usePages, PagesProvider }  from './PagesContext';

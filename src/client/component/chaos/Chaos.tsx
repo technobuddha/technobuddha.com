77e2ad43d85f8948857bf6624a9dd0b0d713a991
Phil Hill <phil@technobuddha.com>
2021-01-22 18:20:09 -0500

    Updated Chaos

--- src/client/component/chaos/Chaos.tsx
+++ src/client/component/chaos/Chaos.tsx
@@ -9,7 +9,7 @@ import css                  from './Chaos.module.css';
 import type { RGBV }        from '#worker/chaos';
 
 const SIZE          = 1;
-const MAX_ITERATION = 256;
+const MAX_ITERATION = 1024;
 
 export const Chaos: React.FC = () => {
     return (
@@ -25,6 +25,7 @@ type Mode = 'compute' | 'display';
 const ChaosBoard: React.FC<ChaosBoardProps> = ({boxWidth, boxHeight}: ChaosBoardProps) => {
     const { t }                 = useTranslation();
     const canvas                = React.useRef<HTMLCanvasElement>(null);
+    const overlay               = React.useRef<HTMLCanvasElement>(null);
     const width                 = React.useMemo(() => Math.floor(boxWidth / SIZE),  [boxWidth]);
     const height                = React.useMemo(() => Math.floor(boxHeight / SIZE), [boxHeight]);
     const [ mode, setMode ]     = useDerivedState<Mode>('compute', [width, height]);
@@ -39,39 +40,88 @@ const ChaosBoard: React.FC<ChaosBoardProps> = ({boxWidth, boxHeight}: ChaosBoard
     const corner                = React.useRef({x: 0, y: 0});
 
     const coordinates = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-        const { top, left, width, height } = canvas.current!.getBoundingClientRect();
-        const clientX = event.clientX - left;
-        const clientY = event.clientY - top;
+        const { top, left } = canvas.current!.getBoundingClientRect();
+        const x = event.clientX - left;
+        const y = event.clientY - top;
+        return {x, y};
+    }
+
+    const scaledCoordinates = ({x: clientX, y: clientY}: {x: number, y: number}) => {
         const x = x_min.current + (clientX / width)  * (x_max.current - x_min.current);
         const y = y_min.current + (clientY / height) * (y_max.current - y_min.current);
         return {x, y};
     }
 
+    const clearOverlay = () => {
+        const context   = overlay.current!.getContext('2d')!;
+        context.clearRect(0, 0, width, height);
+        return context;
+    }
+
     const handleMouseDown       = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if(mode === 'display') {
-            mouseIsDown.current = true;
-            corner.current      = coordinates(event);
+            event.stopPropagation();
+            event.preventDefault();
+
+            if(event.button === 2) {
+                const x_mid = (x_min.current + x_max.current) / 2;
+                const y_mid = (y_min.current + y_max.current) / 2;
+
+                console.log(x_min.current, x_max.current, y_min.current, y_max.current);
+                x_min.current = x_mid - (x_mid - x_min.current) * Math.sqrt(10);
+                x_max.current = x_mid + (x_max.current - x_mid) * Math.sqrt(10);
+                y_min.current = y_mid - (y_mid - y_min.current) * Math.sqrt(10);
+                y_max.current = y_mid + (y_max.current - y_mid) * Math.sqrt(10);
+                console.log(x_min.current, x_max.current, y_min.current, y_max.current);
+                setMode('compute');
+            } else {
+                mouseIsDown.current = true;
+                corner.current      = coordinates(event);
+            }
         }
     }
 
     const handleMouseUp         = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if(mode === 'display') {
             if(mouseIsDown.current) {
-                const { x, y } = coordinates(event);
+                const click  = coordinates(event);
 
-                x_min.current = Math.min(corner.current.x, x);
-                x_max.current = Math.max(corner.current.x, x);
-                y_min.current = Math.min(corner.current.y, y);
-                y_max.current = Math.max(corner.current.y, y);
+                if(Math.abs(click.x - corner.current.x) < 10 || Math.abs(click.y - corner.current.y) < 10) {
+                    clearOverlay();
+                } else {
+                    const first  = scaledCoordinates(corner.current);
+                    const second = scaledCoordinates(click);
+
+                    x_min.current = Math.min(first.x, second.x);
+                    x_max.current = Math.max(first.x, second.x);
+                    y_min.current = Math.min(first.y, second.y);
+                    y_max.current = Math.max(first.y, second.y);
+                    setMode('compute');
+                }
                 mouseIsDown.current = false;
-                setMode('compute');
             }
         }
     };
 
+    const handleMouseMove       = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        if(mode === 'display' && mouseIsDown.current) {
+            const { x, y }  = coordinates(event);
+            const context   = clearOverlay();
+            context.strokeStyle = 'white';
+            context.strokeRect(x, y, corner.current.x - x, corner.current.y - y);
+        }
+    }
+
+    const handleContextMenu     = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        event.preventDefault();
+        return false;
+    }
+
     React.useEffect(
         () => {
             if(mode === 'compute') {
+                clearOverlay();
+
                 chaos.mandelbrot(width, height, x_min.current, x_max.current, y_min.current, y_max.current, MAX_ITERATION)
                 .then(result => {
                     grid.current = result.colors;
@@ -79,7 +129,6 @@ const ChaosBoard: React.FC<ChaosBoardProps> = ({boxWidth, boxHeight}: ChaosBoard
                     x_max.current = result.x_max;
                     y_min.current = result.y_min;
                     y_max.current = result.y_max;
-                    console.log(x_min.current, x_max.current, y_min.current, y_max.current);
                     setMode('display');
                 })
                 .catch(err => console.error(err));
@@ -116,7 +165,7 @@ const ChaosBoard: React.FC<ChaosBoardProps> = ({boxWidth, boxHeight}: ChaosBoard
     )
 
     return (
-        <div className={css.chaos} style={{width: boxWidth, height: height}}>
+        <div className={css.chaos} style={{width: boxWidth, height: height}} onContextMenu={handleContextMenu}>
             {
                 mode === 'compute' &&
                 <div className={css.compute}>
@@ -129,8 +178,18 @@ const ChaosBoard: React.FC<ChaosBoardProps> = ({boxWidth, boxHeight}: ChaosBoard
                     <LinearProgress style={{width: '50%'}} color="primary" />
                 </div>
             }
-            <canvas ref={canvas} className={css.canvas} width={boxWidth} height={boxHeight} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-            </canvas>
+            <canvas ref={canvas} className={css.canvas} width={boxWidth} height={boxHeight} />
+            <canvas
+                ref={overlay}
+                className={css.overlay}
+                width={boxWidth}
+                height={boxHeight}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+                onContextMenu={handleContextMenu}
+            />
+
         </div>
     );
 }

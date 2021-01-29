b810735a2a25a98ef1da89042afcb4a06f0a1eba
Phil Hill <phil@technobuddha.com>
2021-01-28 23:16:48 -0500

    build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,17 +3,19 @@ import fs                           from 'fs-extra';
 import process                      from 'process';
 import webpack                      from 'webpack';
 import chalk                        from 'chalk';
+import shell                        from 'shelljs';
+import metricUnits                  from '@technobuddha/library/metricUnits';
 import binaryUnits                  from '@technobuddha/library/binaryUnits';
 import { genServerWebpackConfig }   from '#server/webpack.config';
 import { genClientWebpackConfig }   from '#client/webpack.config';
 import type { PackageJson }         from 'type-fest';
+chalk.level = 3;
 
 process.env.NODE_ENV = 'production';
 
-chalk.level = 3;    // Tell chalk that we support full RGB colors
-
-function out(text: string) {
-    process.stdout.write(text);
+function out(text: string | undefined) {
+    if(text)
+        process.stdout.write(text);
 }
 
 function report(error: Error, stats: webpack.Stats): void {
@@ -22,7 +24,7 @@ function report(error: Error, stats: webpack.Stats): void {
         process.exit(1);
     }
 
-    out(` ${(stats.endTime ?? 0) - (stats.startTime ?? 0)}ms\n`);
+    // out(` ${(stats.endTime ?? 0) - (stats.startTime ?? 0)}ms\n`);
     if(stats.compilation.errors.length) {
         out(`${chalk.red('Errors:')}\n`);
         for(const e of stats.compilation.errors) {
@@ -42,24 +44,60 @@ function report(error: Error, stats: webpack.Stats): void {
     for(const [filename, asset] of Object.entries(stats.compilation.assets)) {
         const a: any = asset;
 
-        out(`${filename.padEnd(40)} ${(a.isOverSizeLimit ? chalk.red : chalk.green)(binaryUnits(fs.statSync(a.existsAt).size)).padStart(20)}\n`);
+        out(`${' '.repeat(50)}${filename.padEnd(40)} ${(a.isOverSizeLimit ? chalk.red : chalk.green)(binaryUnits(fs.statSync(a.existsAt).size, { format: '###.00', pad: 6 })).padStart(20)}\n`);
     }
 }
 
-out(`Compiling ${chalk.blue('server')}`);
+function run(res: shell.ShellString) {
+    if(res.code !== 0) {
+        if(res.stdout) out(chalk.yellow(res.stdout));
+        if(res.stderr) out(chalk.red(res.stderr));
+        process.exit(res.code);
+    }
+}
+
+let timer: number;
+function start(task: string, text?: string) {
+    out(chalk.whiteBright(task));
+    out(' '.repeat(12 - task.length));
+    if(text) {
+        out(chalk.blue(text));
+        out(' '.repeat(20 - text.length));
+    } else
+        out(' '.repeat(20));
+    timer = Date.now();
+}
+
+function finish() {
+    out(chalk.cyanBright(`${metricUnits((Date.now() - timer) / 1000, { format: '##0.00', pad: 6 })}s\n`));
+}
+
+start('Cleaning');
+run(shell.rm('-rf', 'deploy'));
+run(shell.mkdir('deploy'));
+run(shell.mkdir('deploy/bin'));
+run(shell.mkdir('deploy/dist'));
+finish();
+
+start('Compiling', 'server');
 webpack(
     genServerWebpackConfig(false),
     (errorServer: Error, statsServer: webpack.Stats) => {
+        finish();
         report(errorServer, statsServer);
 
-        out(`\nCompiling ${chalk.blue('client')}`);
+        start('Compiling', 'client');
         webpack(
             genClientWebpackConfig(false),
             (errorClient: Error, statsClient: webpack.Stats) => {
+                finish();
                 report(errorClient, statsClient);
 
-                out(`\nBuilding ${chalk.blue('package.json')}`);
+                start('Copying', 'files');
+                run(shell.cp('-r', [ 'assets', 'webroot', 'data', 'locales', 'views', '.env', 'package/*' ], 'deploy'));
+                finish();
 
+                start('Building', 'package.json');
                 const pj = JSON.parse(fs.readFileSync('package.json').toString()) as PackageJson;
 
                 pj.name = 'gateway';
@@ -79,7 +117,13 @@ webpack(
                 );
 
                 fs.writeFileSync('deploy/package.json', JSON.stringify(pj, undefined, 2), 'utf8');
-                out(`\n--${chalk.blue('done')}\n`);
+                finish();
+
+                start('npm', 'install');
+                shell.exec('npm install --prefix ./deploy', { silent: true });
+                finish();
+
+                out(`\n--${chalk.green('done')}\n`);
                 process.exit(0);
             }
         );
